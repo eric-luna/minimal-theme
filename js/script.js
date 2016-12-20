2333d1801cea094dc46fc70894b44b33fc30dd51
Eric <ericluna1992@gmail.com>
2016-12-20 17:48:54 -0600

    changed navigation menu using only javscript and took out jquery

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-$(document).ready(function(){
-	var check=0;
-	$('.button').on('click',function(){
-		if(check===0){
-			$(".nav-list").addClass('display');
-			check=1;	
-		}else{
-			$(".nav-list").removeClass('display');
-			check=0;
-		}
-	})
-	$('nav-list ul li').on('click',function(){
-		$(".nav-list").removeClass("display");
-	})
-});
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+var expand = document.getElementById("expand");
+expand.addEventListener("click", myFunction, false);
